449ece82031003f59063085a2d6bee286d184759
jacobwu123 <wuchenhao1994@gmail.com>
2018-02-12 03:23:39 -0600

    Finish redirection handling

--- MP1/client.c
+++ MP1/client.c
@@ -22,14 +22,124 @@ void *get_in_addr(struct sockaddr *sa)
 	return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-void messageCat(char* message, char* hostname, char* filename){
+void messageCat(char* message, char* hostname, char* filename, char* portnumber){
 	strcpy(message, "GET ");
 	strcat(message, filename);
-	strcat(message," HTTP/1.1\r\nHost: ");
+	strcat(message," HTTP/1.0\r\nHost: ");
 	strcat(message, hostname);
 	strcat(message,":");
-	strcat(message,PORT);
+	strcat(message,portnumber);
 	strcat(message,"\r\n\r\n");
+}
+int getPortnumber(const char* str, char* portnumber){
+	char* temp;
+	int i = 0;
+
+	temp = strstr(str, ":"); 
+	temp ++;
+	while(1){
+		if(temp[i] == '/'){
+			portnumber[i] = '\0';
+			break;
+		}
+		portnumber[i] = temp[i];
+		i++;
+	}
+	return strlen(portnumber);
+}
+
+void getHost(const char* url, char* hostname, int offset, char* portnumber, 
+				char*filename)
+{
+	int i,j;
+
+	for(i = offset;;i++){
+		hostname[i-offset] = url[i];
+		if(url[i+1] == '/' || url[i+1] == ':'){
+			hostname[i-6] = '\0';
+			break;
+		}
+	}
+
+	if(url[i+1] == ':')
+		i += getPortnumber(&(url[7]), portnumber);
+	else
+		strcpy(portnumber, PORT);
+
+	for(j = i+1; ;j++){
+		filename[j-i-1] = url[j];
+		if(url[j+1] == '\0'){
+			filename[j-i] = '\0';
+			break;
+		}
+	}
+}
+
+int getServer(struct addrinfo* servinfo, struct addrinfo* *p){
+	int sockfd;
+
+	for(*p = servinfo; *p!=NULL;*p= (*p)->ai_next){
+		if((sockfd = socket((*p)->ai_family, (*p)->ai_socktype, 
+			(*p)->ai_protocol)) == -1){
+			perror("client:socket");
+			continue;
+		}
+		if(connect(sockfd, (*p)->ai_addr, (*p)->ai_addrlen)==-1){
+			close(sockfd);
+			perror("client: connect");
+			continue;
+		}
+		break;
+	}
+
+	return sockfd;
+}
+
+int readData(int sockfd, char* url){
+	FILE *fp;
+	char buf[MAXDATASIZE];
+	int isRedirection = 0;
+	char* contents;
+	int count;
+	int i = 0;
+	char* temp; 
+
+	fp = fopen("output","ab");
+	//read the incoming data and find"\r\n\r\n"
+	if((count = recv(sockfd, buf, MAXDATASIZE,0)) >0){
+		if(strstr(buf, "301 Moved Permanently\r\n") != NULL){
+			isRedirection = 1;
+			contents = buf;
+			temp = strstr(buf, "Location: ");
+			temp += strlen("Location: ");
+			while(1){
+				url[i] = temp[i];
+				if(temp[i+1] == '\r'){
+					url[i+1] = '\0';
+					break;
+				} 
+				i++;
+			}
+			printf("%s\n", url);
+		}
+		else{
+			contents = strstr(buf,"\r\n\r\n");
+			contents += 4;
+		}
+		if(fp!= NULL)
+			fwrite(contents,1, count-(contents-buf),fp);
+	}
+
+	while((count = recv(sockfd,buf, MAXDATASIZE,0)) >0){
+		if(fp!=NULL){
+			fwrite(buf,1,count,fp);
+		}
+	}
+	fclose(fp);
+
+	if(isRedirection)
+		remove("output");
+	return isRedirection;
 }
 
 int main(int argc, char const *argv[])
@@ -37,53 +147,29 @@ int main(int argc, char const *argv[])
 	char hostname[MAXDATASIZE];
 	char filename[MAXDATASIZE];
 	char message[MAXDATASIZE];
-	int i,j;
+
 	struct addrinfo hints, *servinfo,*p;
 	int rv;
 	int sockfd;
-	FILE *fp;
-	char buf[MAXDATASIZE];
+	char portnumber[10];
+	char url[MAXDATASIZE];
 
 	if(argc != 2){
 		fprintf(stderr, "usage: client hostname\n");
 	}
 
-	for(i = 7;;i++){
-		hostname[i-7] = argv[1][i];
-		if(argv[1][i+1] == '/')
-			break;
-	}
-
-	for(j = i+1; ;j++){
-		filename[j-i-1] = argv[1][j];
-		if(argv[1][j+1] == '\0')
-			break;
-	}
+	getHost(&argv[1][0], hostname, 7, portnumber, filename);
 
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 
-	if((rv = getaddrinfo(hostname, PORT, &hints, &servinfo))!=0){
+	if((rv = getaddrinfo(hostname, portnumber, &hints, &servinfo))!=0){
 		fprintf(stderr, "getaddrinfo:%s\n",gai_strerror(rv));
 		return 1;
 	}
 
-	for(p = servinfo; p!=NULL;p= p->ai_next){
-		if((sockfd = socket(p->ai_family, p->ai_socktype, 
-			p->ai_protocol)) == -1){
-			perror("client:socket");
-			continue;
-		}
-
-		if(connect(sockfd, p->ai_addr, p->ai_addrlen)==-1){
-			close(sockfd);
-			perror("client: connect");
-			continue;
-		}
-
-		break;
-	}
+	sockfd = getServer(servinfo, &p);
 
 	if(p == NULL){
 		fprintf(stderr, "client: fail to connect\n");
@@ -91,16 +177,36 @@ int main(int argc, char const *argv[])
 	}
 
 	freeaddrinfo(servinfo);
-	messageCat(message, hostname, filename);
+	messageCat(message, hostname, filename, portnumber);
 	
 	if(send(sockfd, message, strlen(message),0)== -1) perror("send");
+	
+	while(readData(sockfd, &(url[0])) == 1){
+		printf("reach here\n");
+		printf("%s\n", url);
+		memset(&hostname[0], 0, sizeof(hostname));
+		memset(&portnumber[0], 0, sizeof(portnumber));
+		memset(&filename[0], 0, sizeof(filename));
 
-	fp = fopen("output","ab");
-	while(recv(sockfd,buf, MAXDATASIZE-1,0) >0){
-		if(fp!=NULL){
-			fputs(buf,fp);
+		getHost(url, hostname, 7, portnumber, filename);
+		memset(&hints, 0, sizeof hints);
+		hints.ai_family = AF_UNSPEC;
+		hints.ai_socktype = SOCK_STREAM;
+
+		if((rv = getaddrinfo(hostname, portnumber, &hints, &servinfo))!=0){
+			fprintf(stderr, "getaddrinfo:%s\n",gai_strerror(rv));
+			return 1;
 		}
+		sockfd = getServer(servinfo, &p);
+		if(p == NULL){
+			fprintf(stderr, "client: fail to connect\n");
+			return 2;
+		}
+		freeaddrinfo(servinfo);
+		memset(&message[0], 0, sizeof(message));
+		messageCat(message, hostname, filename, portnumber);
+		if(send(sockfd, message, strlen(message),0)== -1) perror("send");
 	}
-	fclose(fp);
+
 	return 0;
 }
--- MP1/server.c
+++ MP1/server.c
@@ -1,5 +1,173 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define BACKLOG 10
+#define MAX_DATA_SIZE 1024 
+void sigchld_handler(int s)
+{
+	while(waitpid(-1, NULL, WNOHANG) > 0);
+}
+void *get_in_addr(struct sockaddr *sa)
+{
+	if(sa->sa_family == AF_INET){
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int parseMessage(char* filename, char*buf){
+	char* temp;
+	int i = 0;
+
+	temp = strstr(buf, "GET ");
+
+	if(temp != NULL || strstr(buf, "HTTP/1.") !=NULL || strstr(buf, "\r\n\r\n") != NULL){
+		temp += 6;
+		while(1){
+			if(temp[i] == ' '){
+				filename[i] = '\0';
+				break;
+			}
+			filename[i] = temp[i];
+			i++;
+		}
+	}
+	else{
+		return 0;
+	}
+	return 1;
+}
+
 int main(int argc, char const *argv[])
 {
-	
+	struct addrinfo hints, *servinfo, *p;
+	int sockfd, new_fd;
+	int rv;
+	int yes =1;
+	struct sigaction sa;
+	struct sockaddr_storage their_addr;
+	socklen_t sin_size;
+	char s[INET6_ADDRSTRLEN];
+	char buf[MAX_DATA_SIZE];
+	int numbytes;
+	char filename[MAX_DATA_SIZE];
+	FILE* fp;
+	int count;
+	char returnMessage[20];
+
+	if(argc != 2){
+		fprintf(stderr, "usage: server portnumber\n");
+	}
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	if((rv = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0){
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	for(p =  servinfo; p!= NULL; p = p->ai_next){
+		if((sockfd = socket(p->ai_family, p->ai_socktype, 
+				p->ai_protocol)) == -1){
+			perror("server: socket");
+			continue;
+		}
+
+		if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT|SO_REUSEADDR, &yes, 
+				sizeof(int)) == -1){
+			perror("setsockopt");
+			continue;
+		}
+			
+		if(bind(sockfd, p->ai_addr, p->ai_addrlen) == -1){
+			close(sockfd);
+			perror("server:bind");
+			continue;
+		}
+
+		break;
+	}
+
+	if(p == NULL){
+		fprintf(stderr, "server: failed to bind\n");
+		return 2;
+	}
+
+	freeaddrinfo(servinfo);
+
+	if(listen(sockfd, BACKLOG) == -1){
+		perror("listen");
+		exit(1);
+	}
+
+	sa.sa_handler = sigchld_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART;
+	if(sigaction(SIGCHLD, &sa, NULL) == -1){
+		perror("sigaction");
+		exit(1);
+	}
+
+	printf("server: waiting for connections...\n");
+
+	while(1){
+		sin_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+		if(new_fd == -1){
+			perror("accept");
+			continue;
+		}
+
+		inet_ntop(their_addr.ss_family,
+			get_in_addr((struct sockaddr *)&their_addr),
+			s, sizeof s);
+		printf("server: got connection from %s\n", s);
+
+		if(!fork()){
+			close(sockfd);
+			if((numbytes = recv(new_fd, buf, MAX_DATA_SIZE-1, 0)) == -1){
+				perror("recv");
+				exit(1);
+			}
+	    	if(parseMessage(filename, buf)){
+	    		if((fp =fopen(filename, "r")) != NULL)
+	    			strcpy(returnMessage, "HTTP/1.0 200 OK\r\n\r\n");
+	    		else
+	    			strcpy(returnMessage, "HTTP/1.0 404 Not Found\r\n\r\n");
+	    	}
+	    	else{
+	    		strcpy(returnMessage, "HTTP/1.0 400 Bad Request\r\n\r\n");
+	    	}
+			//send returnMessages
+			if(send(new_fd, returnMessage, strlen(returnMessage),0) == -1){
+				perror("send");
+			}
+			while((count = fread(buf, 1, MAX_DATA_SIZE, fp)) > 0){
+				printf("%d\n", count);
+				if(send(new_fd, buf, count,0) == -1){
+					perror("send");
+				}
+			}
+			fclose(fp);
+			close(new_fd);
+			exit(0);
+		}
+		close(new_fd);
+
+	}
+
 	return 0;
 }
